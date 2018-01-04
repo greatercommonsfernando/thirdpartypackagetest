6b422ab54dd7ac57bc060791c51eb6d4cf0f592e
fernando <fernando@bixly.com>
2018-01-04 13:26:57 -0800

    -Imported github.com/olebedev/config -Added code to read a YAML file

--- main.go
+++ main.go
@@ -1,7 +1,19 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"github.com/olebedev/config"
+)
 
 func main() {
+	if cfg, err := config.ParseYamlFile("config.yml"); err != nil {
+		panic(err)
+	} else {
+		if testValue, err := cfg.String("test"); err != nil {
+			panic(err)
+		} else {
+			fmt.Println("test:", testValue)
+		}
+	}
 	fmt.Println("Hello World!")
 }
